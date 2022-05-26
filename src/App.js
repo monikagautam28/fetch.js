6f681f638f435d89ba61c553472e9605537e81b5
Monika Gautam <monika28may@gmail.com>
2022-05-26 15:42:25 +0530

    code merge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Fetch from './Fetch';
+import List from './List'
 
 function App() {
+
+ let data=[{
+    country:'taiwan'
+  },
+  {
+    country:'canada'
+  },
+  {
+    country:'india'
+  },
+  {
+    country:'dubai'
+  },
+  {
+    country:'australia'
+  },
+]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <Fetch/>
+  <List mylist ={data}/>
     </div>
   );
 }
